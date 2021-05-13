272ec5ac56612a50e3c13a4ab3058cc24aa2d9eb
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-05-13 06:50:20 +0100

    file metadata

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +13,14 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload.single('upfile'), (req, res) => {
+    let fileDetails = req.file
+    let name = fileDetails.originalname
+    let type = fileDetails.mimetype
+    let size = fileDetails.size
+
+    res.json({name, type, size})
+})
 
 
 
